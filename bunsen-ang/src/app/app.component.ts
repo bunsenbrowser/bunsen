7f03b468509a4fcb58b399e035ccb96454069393
Chris Kelley <chris@vetula.com>
2017-08-02 12:15:35 +0200

    Assets are copied over to device fixes #8

--- bunsen-ang/src/app/app.component.ts
+++ bunsen-ang/src/app/app.component.ts
@@ -24,57 +24,40 @@ export class AppComponent {
   ngOnInit() {
     document.addEventListener('deviceready', () => {
       console.log("deviceready");
-      // console.log('Using Cordova plugins with Angular 2. Cordova version: ' + device.cordova);
-      console.log(cordova.file);
-      window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, createDirsFs, onErrorLoadFs);
+      // console.log(cordova.file);
 
-      function createDirsFs(fs) {
-        console.log('file system open: ' + fs.name);
-        // var cache = getCacheDir().getAbsolutePath();
-        // var jsPath = cache + "/node";
-        // var corePath = cache + "/node_modules";
-        var entry=fs.root;
-        // entry.getDirectory("node", {create: true, exclusive: false}, onGetDirectorySuccess, onGetDirectoryFail).then(
-        //   entry.getDirectory("node_modules", {create: true, exclusive: false}, onGetDirectorySuccess, onGetDirectoryFail).then(createCopyDir(fs, 'node'))
-        createCopyDir(fs, 'node')
-        // )
+      // Gain access to the main user file system.
+      window.resolveLocalFileSystemURL(cordova.file.externalRootDirectory, createDirs, onErrorLoadFs);
+
+      // Create bunsen dir and copy subdirs
+      function createDirs(rootDirEntry) {
+        rootDirEntry.getDirectory('bunsen', { create: true }, function (entry) {
+          console.log("Created dir at " + entry.toURL());
+          createCopyDir(entry, 'node');
+          createCopyDir(entry, 'node_modules');
+        })
       }
 
-      function createCopyDir(fs, dir) {
-        // app.fileSystem = fs;
-        var url = cordova.file.applicationDirectory+"index.js";
+      // create/copy dirs from assets dir.
+      function createCopyDir(rootDirEntry, dir) {
+        var url = cordova.file.applicationDirectory+"www/assets/" + dir;
         console.log("url: " + url)
         window.resolveLocalFileSystemURL(url, function(entry) {
-          // var fullPath = "node";
-          fs.root.getDirectory(dir, {create:true, exclusive:false}, function(entryDirectory) {
-            entry.copyTo(entryDirectory, dir, function(rs) {
+            entry.copyTo(rootDirEntry, null, function(rs) {
               console.log(JSON.stringify(rs)); //success
-            }, function(rs) { console.log("1 "+JSON.stringify(rs));} );
-          }, function(rs) { console.log("2 "+JSON.stringify(rs));} );
-        }, function(rs) { console.log("3 "+JSON.stringify(rs));} );
+            }, function(rs) { console.log("Error copying to " + dir + ' result:'+JSON.stringify(rs));} );
+        }, function(rs) { console.log("Error in resolveLocalFileSystemURL result for " + dir + ": "+JSON.stringify(rs));} );
       }
-
-      // window.resolveLocalFileSystemURL(cordova.file.applicationDirectory, function (dirEntry) {
-      //   console.log('file system open: ' + dirEntry.name);
-      //   var isAppend = true;
-      //   createFile(dirEntry, "fileToAppend.txt", isAppend);
-      // }, onErrorLoadFs);
 
       function onErrorLoadFs(error) {
         console.log("Error loading filesystem "+ error.code);
       }
-      function onGetDirectorySuccess(dir) {
-        console.log("Created dir: "+ dir);
-      }
-      function onGetDirectoryFail(error) {
-        console.log("Error creating dir "+ error.code);
-      }
+
       CordovaNodePlugin.startServer(function (result) {
         console.log('Result of starting Node: ' + result);
       }, function (err) {
         console.log(err);
       });
-    // CordovaNodePlugin.startServer();
     }, false);
   }
 
