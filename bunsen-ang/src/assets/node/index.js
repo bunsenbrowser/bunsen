638484e138e84779f7773b5d4fdda21d1422dfd6
Chris Kelley <chris@vetula.com>
2017-08-08 17:16:36 +0200

    displays directory listing
    added CSP permissions

--- bunsen-ang/src/assets/node/index.js
+++ bunsen-ang/src/assets/node/index.js
@@ -15,7 +15,18 @@ var swarm = require('discovery-swarm')
 var defaults = require('datland-swarm-defaults')
 var minimist = require('minimist')
 var ram = require('random-access-memory')
-var hyperdriveHttp = require('hyperdrive-http')
+// var hyperdriveHttp = require('hyperdrive-http')
+var toHTML = require('directory-index-html')
+var pump = require('pump')
+var mime = require('mime')
+const express = require('express')
+var fs = require('fs');
+var path = require('path')
+var Dat = require('dat-node')
+var mirror = require('mirror-folder')
+
+
+
 
 // var android = function () { // TODO: move to internal module
 //     var port = Number(process.argv[process.argv.length - 1])
@@ -33,7 +44,7 @@ console.log('argv', process.argv)
 
 
 // var link = "dat://778f8d955175c92e4ced5e4f5563f69bfec0c86cc6f670352c457943666fe639"
-var link = "778f8d955175c92e4ced5e4f5563f69bfec0c86cc6f670352c457943666fe639"
+// var link = "778f8d955175c92e4ced5e4f5563f69bfec0c86cc6f670352c457943666fe639"
 // var archiveText = "";
 // var archive = hyperdrive(ram, link)
 // archive.ready(function (err) {
@@ -79,24 +90,26 @@ var link = "778f8d955175c92e4ced5e4f5563f69bfec0c86cc6f670352c457943666fe639"
 
 // server.listen(8080);
 // console.log("Server is listening");
-
+//
 // var argv = minimist(process.argv.slice(2), {
 //   alias: {port: 'p', cacheSize: 'cache-size'},
 //   default: {port: process.env.PORT || 8080, db: 'dat.haus.db'}
 // })
-//
+// //
 // var db = ram
 // var drive = hyperdrive(db)
 //
 // var sw = swarm(defaults({
 //   hash: false,
 //   stream: function (info) {
+//     console.log("In the stream")
 //     var stream = drive.replicate()
 //     if (info.channel) join(info.channel) // we already know the channel, join
 //     else stream.once('open', join) // wait for the remote to tell us
 //     return stream
 //
 //     function join (discoveryKey) {
+//       console.log("In the stream, joining discoveryKey" + discoveryKey)
 //       var archive = cache.get(discoveryKey.toString('hex'))
 //       if (archive) archive.replicate({stream: stream})
 //     }
@@ -126,27 +139,268 @@ var link = "778f8d955175c92e4ced5e4f5563f69bfec0c86cc6f670352c457943666fe639"
 // })
 //
 // function getArchive(dat, cb) {
-//   console.log("Getting dat: " + dat);
+//   console.log("Getting dat: " + dat + " for cb: " + cb);
 //   var archive = cache.get(dat.discoveryKey)
 //   if (!archive) {
+//     console.log("getArchive createArchive")
 //     archive = drive.createArchive(dat.key, {file: file})
+//     // archive = drive.replicate(dat.key, {file: file})
+//   //   archive = hyperdrive(ram, link, {sparse: true})
 //     cache.set(archive.discoveryKey.toString('hex'), archive)
 //     sw.join(archive.discoveryKey)
 //   }
 //   cb(null, archive)
+//   return archive
 // }
 
-var key = link;
-var storage = ram
-var port = 8080
 
-var archive = hyperdrive(storage, key, {sparse: true})
-var server = http.createServer(hyperdriveHttp(archive, {live: true}))
-server.listen(port)
-console.log(`Visit http://localhost:${port} to see archive`)
+// var key = link;
+// var storage = ram
+// var port = 8080
+//
+// var archive = hyperdrive(storage, key, {sparse: true})
+// var server = http.createServer(hyperdriveHttp(archive, {live: true}))
+// server.listen(port)
+// console.log(`Visit http://localhost:${port} to see archive`)
+//
+// if (key) {
+//   archive.ready(function () {
+//     discovery(archive, {live: true})
+//   })
+// }
 
-if (key) {
-  archive.ready(function () {
-    discovery(archive, {live: true})
+var opts = {}
+var app = express()
+app.set('port', process.env.PORT || 8080);
+app.get('/', function( req, res) {
+  res.send('hello world');
+});
+var dat = ""
+Bunsen = {}
+app.get('/dat/:dat', function(req, res) {
+  // var name = decodeURI(req.url.split('/')[0])
+  var name = '/';
+  var dat = req.params.dat
+  console.log("name: " + name + " dat: " + dat)
+  // res.send("dat is set to " + req.params.dat);
+  // var link = "778f8d955175c92e4ced5e4f5563f69bfec0c86cc6f670352c457943666fe639"
+  var archiveText = "";
+  console.log("cwp: " + process.cwd())
+  var dir = "www"
+  if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir)
+  } {
+    deleteFolderRecursive(dir)
+    fs.mkdirSync(dir)
+  }
+
+  var dest = path.join(__dirname, 'www')
+  // 1. Tell Dat where to download the files
+  Dat(ram, {key: dat, sparse: true}, function (err, dat) {
+    if (err) throw err
+
+    var network = dat.joinNetwork()
+    network.once('connection', function () {
+      console.log('Connected')
+    })
+    dat.archive.metadata.update(download)
+
+    function download () {
+      var progress = mirror({fs: dat.archive, name: '/'}, dest, function (err) {
+        if (err) throw err
+        console.log('Done')
+      })
+      progress.on('put', function (src) {
+        console.log('Downloading', src.name)
+      })
+      progress.on('end', function () {
+        console.log('Finished downloading')
+      })
+
+    }
+
+    console.log(`Downloading: ${dat.key.toString('hex')}\n`)
+  })
+  var archive = hyperdrive(ram, dat)
+  // var archive = hyperdrive('./www', dat)
+  // var archive = hyperdrive('./www', dat, {sparse: true})
+  archive.ready(function (err) {
+    if (err) throw err
+    Bunsen.archive = archive
+    console.log('key', archive.key.toString('hex'))
+    var sw = discovery(archive)
+    sw.on('connection', function (peer, type) {
+      // console.log('got', peer, type) // type is 'webrtc-swarm' or 'discovery-swarm'
+      // console.log('connected to', sw.connections, 'peers')
+      peer.on('close', function () {
+        console.log('peer disconnected')
+      })
+    })
+    // console.log("Trying to clone via dat.")
+    // clone.add(key, function (repo) {
+    //   archive.readFile('dat.json', function (err, data) {
+    //     console.log("dat.json: " + data.toString()) // prints 'world'
+    //   })
+    // })
+
+    // archive.replicate({download:true})
+    var stream = archive.readFile('index.html', 'utf-8', function (err, data) {
+      if (err) throw err
+      archiveText = data;
+      console.log(archiveText)
+      // res.send(archiveText);
+
+      var list = archive.readdir('/', function (err, list) {
+        console.log("\n\n archive list: " + list + " err: " + err)
+        ondirectory(archive, name, req, res, opts)
+      });
+    })
+    // archive.metadata.on('download', (idx, data) => console.log('download', idx, data))
+
+
+    // var wait = (query.wait && Number(query.wait.toString())) || 0
+    // var have = archive.metadata ? archive.metadata.length : -1
+    //
+    // if (wait <= have) return ready()
+    // waitFor(archive, wait, ready)
+
+    // if (name[name.length - 1] === '/') ondirectory(archive, name, req, res, opts)
+    // else onfile(archive, name, req, res)
+
+  })
+});
+
+app.listen(8080);
+
+
+function onfile (archive, name, req, res) {
+  archive.stat(name, function (err, st) {
+    if (err) return onerror(res, 404, err)
+
+    if (st.isDirectory()) {
+      res.statusCode = 302
+      res.setHeader('Location', name + '/')
+      return
+    }
+
+    var r = req.headers.range && range(st.size, req.headers.range)[0]
+    res.setHeader('Accept-Ranges', 'bytes')
+    res.setHeader('Content-Type', mime.lookup(name))
+
+    if (r) {
+      res.statusCode = 206
+      res.setHeader('Content-Range', 'bytes ' + r.start + '-' + r.end + '/' + st.size)
+      res.setHeader('Content-Length', r.end - r.start + 1)
+    } else {
+      res.setHeader('Content-Length', st.size)
+    }
+
+    if (req.method === 'HEAD') return res.end()
+    pump(archive.createReadStream(name, r), res)
   })
 }
+
+function ondirectory (archive, name, req, res, opts) {
+  console.log("ondirectory")
+    archive.stat(name + 'index.html', function (err) {
+    if (err) return ondirectoryindex(archive, name, req, res, opts)
+    // onfile(archive, name + 'index.html', req, res)
+      ondirectoryindex(archive, name, req, res, opts)
+  })
+}
+
+function ondirectoryindex (archive, name, req, res, opts) {
+  console.log("ondirectoryindex")
+  list(archive, name, function (err, entries) {
+    if (err) entries = []
+    console.log("ondirectoryindex entries: " + entries + " error: " + err)
+    var wait = archive.metadata ? archive.metadata.length + 1 : 0
+    var script = `
+      function liveUpdate () {
+        var xhr = new XMLHttpRequest()
+        xhr.open("GET", "${name}?wait=${wait}", true)
+        xhr.onload = function () {
+          if (xhr.status !== 200) return onerror()
+          document.open()
+          document.write(xhr.responseText)
+          document.close()
+        }
+        xhr.onerror = onerror
+        xhr.send(null)
+        function onerror () {
+          setTimeout(liveUpdate, 1000)
+        }
+      }
+      liveUpdate()
+    `
+
+    var footer = opts.footer ? opts.footer + ' Archive version: ' + archive.version : null
+    var html = toHTML({directory: name, script: (!opts.live || archive._checkout) ? null : script, footer: footer}, entries)
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    res.setHeader('Content-Length', Buffer.byteLength(html))
+    if (opts.exposeHeaders) {
+      res.setHeader('Hyperdrive-Key', archive.key.toString('hex'))
+      res.setHeader('Hyperdrive-Version', archive.version)
+      res.setHeader('Hyperdrive-Http-Version', pkg.version)
+    }
+    res.end(html)
+  })
+}
+
+function onerror (res, status, err) {
+  console.log("error: " + err)
+  res.statusCode = status
+  res.end(err.stack)
+}
+
+function list (archive, name, cb) {
+  archive.readdir(name, function (err, names) {
+    console.log("names: " + names + " err: " + err)
+    if (err) return cb(err)
+
+    var error = null
+    var missing = names.length
+    var entries = []
+
+    if (!missing) return cb(null, [])
+    for (var i = 0; i < names.length; i++) stat(name + names[i], names[i])
+
+    function stat (name, base) {
+      archive.stat(name, function (err, st) {
+        if (err) error = err
+
+        if (st) {
+          entries.push({
+            type: st.isDirectory() ? 'directory' : 'file',
+            name: base,
+            size: st.size,
+            mtime: st.mtime
+          })
+        }
+
+        if (--missing) return
+        if (error) return cb(error)
+        cb(null, entries.sort(sort))
+      })
+    }
+  })
+}
+
+function sort (a, b) {
+  return a.name.localeCompare(b.name)
+}
+
+var deleteFolderRecursive = function(path) {
+  if( fs.existsSync(path) ) {
+    fs.readdirSync(path).forEach(function(file,index){
+      var curPath = path + "/" + file;
+      if(fs.lstatSync(curPath).isDirectory()) { // recurse
+        deleteFolderRecursive(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(path);
+  }
+};
+
--- www/assets/node/index.js
+++ www/assets/node/index.js
@@ -15,7 +15,12 @@ var swarm = require('discovery-swarm')
 var defaults = require('datland-swarm-defaults')
 var minimist = require('minimist')
 var ram = require('random-access-memory')
-var hyperdriveHttp = require('hyperdrive-http')
+// var hyperdriveHttp = require('hyperdrive-http')
+var toHTML = require('directory-index-html')
+var pump = require('pump')
+var mime = require('mime')
+const express = require('express')
+
 
 // var android = function () { // TODO: move to internal module
 //     var port = Number(process.argv[process.argv.length - 1])
@@ -33,7 +38,7 @@ console.log('argv', process.argv)
 
 
 // var link = "dat://778f8d955175c92e4ced5e4f5563f69bfec0c86cc6f670352c457943666fe639"
-var link = "778f8d955175c92e4ced5e4f5563f69bfec0c86cc6f670352c457943666fe639"
+// var link = "778f8d955175c92e4ced5e4f5563f69bfec0c86cc6f670352c457943666fe639"
 // var archiveText = "";
 // var archive = hyperdrive(ram, link)
 // archive.ready(function (err) {
@@ -79,24 +84,26 @@ var link = "778f8d955175c92e4ced5e4f5563f69bfec0c86cc6f670352c457943666fe639"
 
 // server.listen(8080);
 // console.log("Server is listening");
-
+//
 // var argv = minimist(process.argv.slice(2), {
 //   alias: {port: 'p', cacheSize: 'cache-size'},
 //   default: {port: process.env.PORT || 8080, db: 'dat.haus.db'}
 // })
-//
+// //
 // var db = ram
 // var drive = hyperdrive(db)
 //
 // var sw = swarm(defaults({
 //   hash: false,
 //   stream: function (info) {
+//     console.log("In the stream")
 //     var stream = drive.replicate()
 //     if (info.channel) join(info.channel) // we already know the channel, join
 //     else stream.once('open', join) // wait for the remote to tell us
 //     return stream
 //
 //     function join (discoveryKey) {
+//       console.log("In the stream, joining discoveryKey" + discoveryKey)
 //       var archive = cache.get(discoveryKey.toString('hex'))
 //       if (archive) archive.replicate({stream: stream})
 //     }
@@ -126,27 +133,212 @@ var link = "778f8d955175c92e4ced5e4f5563f69bfec0c86cc6f670352c457943666fe639"
 // })
 //
 // function getArchive(dat, cb) {
-//   console.log("Getting dat: " + dat);
+//   console.log("Getting dat: " + dat + " for cb: " + cb);
 //   var archive = cache.get(dat.discoveryKey)
 //   if (!archive) {
+//     console.log("getArchive createArchive")
 //     archive = drive.createArchive(dat.key, {file: file})
+//     // archive = drive.replicate(dat.key, {file: file})
+//   //   archive = hyperdrive(ram, link, {sparse: true})
 //     cache.set(archive.discoveryKey.toString('hex'), archive)
 //     sw.join(archive.discoveryKey)
 //   }
 //   cb(null, archive)
+//   return archive
 // }
 
-var key = link;
-var storage = ram
-var port = 8080
 
-var archive = hyperdrive(storage, key, {sparse: true})
-var server = http.createServer(hyperdriveHttp(archive, {live: true}))
-server.listen(port)
-console.log(`Visit http://localhost:${port} to see archive`)
+// var key = link;
+// var storage = ram
+// var port = 8080
+//
+// var archive = hyperdrive(storage, key, {sparse: true})
+// var server = http.createServer(hyperdriveHttp(archive, {live: true}))
+// server.listen(port)
+// console.log(`Visit http://localhost:${port} to see archive`)
+//
+// if (key) {
+//   archive.ready(function () {
+//     discovery(archive, {live: true})
+//   })
+// }
 
-if (key) {
-  archive.ready(function () {
-    discovery(archive, {live: true})
+var opts = {}
+var app = express()
+app.set('port', process.env.PORT || 8080);
+app.get('/', function( req, res) {
+  res.send('hello world');
+});
+var dat = ""
+app.get('/dat/:dat', function(req, res) {
+  // var name = decodeURI(req.url.split('/')[0])
+  var name = '/';
+  var dat = req.params.dat
+  console.log("name: " + name + " dat: " + dat)
+  // res.send("dat is set to " + req.params.dat);
+  // var link = "778f8d955175c92e4ced5e4f5563f69bfec0c86cc6f670352c457943666fe639"
+  var archiveText = "";
+  var archive = hyperdrive(ram, dat)
+  archive.ready(function (err) {
+    if (err) throw err
+    console.log('key', archive.key.toString('hex'))
+    var sw = discovery(archive)
+    sw.on('connection', function (peer, type) {
+      // console.log('got', peer, type) // type is 'webrtc-swarm' or 'discovery-swarm'
+      // console.log('connected to', sw.connections, 'peers')
+      peer.on('close', function () {
+        console.log('peer disconnected')
+      })
+    })
+    // console.log("Trying to clone via dat.")
+    // clone.add(key, function (repo) {
+    //   archive.readFile('dat.json', function (err, data) {
+    //     console.log("dat.json: " + data.toString()) // prints 'world'
+    //   })
+    // })
+    var stream = archive.readFile('index.html', 'utf-8', function (err, data) {
+      if (err) throw err
+      archiveText = data;
+      console.log(archiveText)
+      // res.send(archiveText);
+
+      var list = archive.readdir('/', function (err, list) {
+        console.log("\n\n archive list: " + list + " err: " + err)
+        ondirectory(archive, name, req, res, opts)
+      });
+    })
+    // archive.metadata.on('download', (idx, data) => console.log('download', idx, data))
+
+
+    // var wait = (query.wait && Number(query.wait.toString())) || 0
+    // var have = archive.metadata ? archive.metadata.length : -1
+    //
+    // if (wait <= have) return ready()
+    // waitFor(archive, wait, ready)
+
+    // if (name[name.length - 1] === '/') ondirectory(archive, name, req, res, opts)
+    // else onfile(archive, name, req, res)
+
+  })
+});
+
+app.listen(8080);
+
+
+function onfile (archive, name, req, res) {
+  archive.stat(name, function (err, st) {
+    if (err) return onerror(res, 404, err)
+
+    if (st.isDirectory()) {
+      res.statusCode = 302
+      res.setHeader('Location', name + '/')
+      return
+    }
+
+    var r = req.headers.range && range(st.size, req.headers.range)[0]
+    res.setHeader('Accept-Ranges', 'bytes')
+    res.setHeader('Content-Type', mime.lookup(name))
+
+    if (r) {
+      res.statusCode = 206
+      res.setHeader('Content-Range', 'bytes ' + r.start + '-' + r.end + '/' + st.size)
+      res.setHeader('Content-Length', r.end - r.start + 1)
+    } else {
+      res.setHeader('Content-Length', st.size)
+    }
+
+    if (req.method === 'HEAD') return res.end()
+    pump(archive.createReadStream(name, r), res)
   })
 }
+
+function ondirectory (archive, name, req, res, opts) {
+  console.log("ondirectory")
+    archive.stat(name + 'index.html', function (err) {
+    if (err) return ondirectoryindex(archive, name, req, res, opts)
+    // onfile(archive, name + 'index.html', req, res)
+      ondirectoryindex(archive, name, req, res, opts)
+  })
+}
+
+function ondirectoryindex (archive, name, req, res, opts) {
+  console.log("ondirectoryindex")
+  list(archive, name, function (err, entries) {
+    if (err) entries = []
+    console.log("ondirectoryindex entries: " + entries + " error: " + err)
+    var wait = archive.metadata ? archive.metadata.length + 1 : 0
+    var script = `
+      function liveUpdate () {
+        var xhr = new XMLHttpRequest()
+        xhr.open("GET", "${name}?wait=${wait}", true)
+        xhr.onload = function () {
+          if (xhr.status !== 200) return onerror()
+          document.open()
+          document.write(xhr.responseText)
+          document.close()
+        }
+        xhr.onerror = onerror
+        xhr.send(null)
+        function onerror () {
+          setTimeout(liveUpdate, 1000)
+        }
+      }
+      liveUpdate()
+    `
+
+    var footer = opts.footer ? opts.footer + ' Archive version: ' + archive.version : null
+    var html = toHTML({directory: name, script: (!opts.live || archive._checkout) ? null : script, footer: footer}, entries)
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    res.setHeader('Content-Length', Buffer.byteLength(html))
+    if (opts.exposeHeaders) {
+      res.setHeader('Hyperdrive-Key', archive.key.toString('hex'))
+      res.setHeader('Hyperdrive-Version', archive.version)
+      res.setHeader('Hyperdrive-Http-Version', pkg.version)
+    }
+    res.end(html)
+  })
+}
+
+function onerror (res, status, err) {
+  console.log("error: " + err)
+  res.statusCode = status
+  res.end(err.stack)
+}
+
+function list (archive, name, cb) {
+  archive.readdir(name, function (err, names) {
+    console.log("names: " + names + " err: " + err)
+    if (err) return cb(err)
+
+    var error = null
+    var missing = names.length
+    var entries = []
+
+    if (!missing) return cb(null, [])
+    for (var i = 0; i < names.length; i++) stat(name + names[i], names[i])
+
+    function stat (name, base) {
+      archive.stat(name, function (err, st) {
+        if (err) error = err
+
+        if (st) {
+          entries.push({
+            type: st.isDirectory() ? 'directory' : 'file',
+            name: base,
+            size: st.size,
+            mtime: st.mtime
+          })
+        }
+
+        if (--missing) return
+        if (error) return cb(error)
+        cb(null, entries.sort(sort))
+      })
+    }
+  })
+}
+
+function sort (a, b) {
+  return a.name.localeCompare(b.name)
+}
+
