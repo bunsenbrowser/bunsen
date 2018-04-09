ea7783617e48203ff9ab1df72fe924385f3e2ae3
R.J. Steinert <rj@rjsteinert.com>
2018-04-09 18:10:52 -0400

    Use a shotCode map to in lvh.me subdomains to avoid character limits

--- server/index.js
+++ server/index.js
@@ -7,20 +7,28 @@ const path = require('path')
 // Set up app.
 var app = express();
 
-// Intercept all routes.
-app.all('*', function (req, res) {
-  const subdomain = req.headers.host.split('.')[0]
-  return express.static(path.join(__dirname, 'archives', subdomain)).apply(this, arguments);
-});
+var map = {}
 
-app.get('/add/:uuid', async function (req, res) {
+app.get('/open/:uuid', async function (req, res) {
+  console.log('hi')
   try {
     await exec(`cd archives && dat clone ${req.params.uuid}`)
   } catch (e) {
     return res.send(e)
   }
+  const shortName = req.params.uuid.substr(0, 6)
+  map[shortName] = req.params.uuid
+  res.redirect(`http://${shortName}.lvh.me`)
   res.send('ok')
 })
+
+// Intercept all routes.
+app.all('*', function (req, res) {
+  // Get shortname from subdomain on *.lvh.me.
+  const shortName = req.headers.host.split('.')[0]
+  const folder = map[shortName]
+  return express.static(path.join(__dirname, 'archives', folder)).apply(this, arguments);
+});
 
 // Listen to on port 80.
 app.listen(80, function () {
