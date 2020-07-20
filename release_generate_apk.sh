0e136428346191f097edeeed3172e7cba9cfcfaa
Chris E. Kelley <chris@vetula.com>
2020-07-20 17:25:02 -0500

    added minSdkVersion 24

--- release_generate_apk.sh
+++ release_generate_apk.sh
@@ -1,3 +1,3 @@
 adb uninstall org.bunsenbrowser
-export ORG_GRADLE_PROJECT_cdvMinSdkVersion=26
+export ORG_GRADLE_PROJECT_cdvMinSdkVersion=24
 cordova build android --release -- --buildConfig build.json
